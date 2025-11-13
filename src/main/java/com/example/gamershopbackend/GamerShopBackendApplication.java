c05f07a936da3bbc229ff06ed788570f2edb7473
W610-PC27 <labinf@PMAWSW6LC0927.duoc.acad>
2025-11-13 15:27:26 -0300

    first commit

---
+++ src/main/java/com/example/gamershopbackend/GamerShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamershopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamerShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamershopbackend/GamerShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamershopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
